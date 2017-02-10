4166c477a5d95e789058f6df6552cad89084ec7e
Adrian PALUMBO <pa506143@etu.unice.fr>
2017-02-10 15:40:26 +0100

    Added "lower than to greater than" & "greater than to lower than" processor

---
+++ devops/src/main/java/spoon/processors/GTtoLT.java
@@ -0,0 +1,17 @@
+package spoon.processors;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.BinaryOperatorKind;
+import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtVariable;
+
+/**
+ * Created by Adrian on 10/02/2017.
+ */
+public class GTtoLT extends AbstractProcessor<CtBinaryOperator> {
+    public void process(CtBinaryOperator elem) {
+        if (elem.getKind() == BinaryOperatorKind.GT)
+            elem.setKind(BinaryOperatorKind.LT);
+    }
+}
