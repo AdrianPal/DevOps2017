aa5a759b2a148d704ded6ecdc872d244748df5bc
Adrian PALUMBO <pa506143@etu.unice.fr>
2017-02-17 15:56:36 +0100

    UPDATE: LTtoGT processor, added the ability to change the "lower than" WHEN we are not in a loop.

--- devops/src/main/java/spoon/processors/LTtoGT.java
+++ devops/src/main/java/spoon/processors/LTtoGT.java
@@ -3,6 +3,7 @@ package spoon.processors;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.BinaryOperatorKind;
 import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.code.CtLoop;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtVariable;
 
@@ -10,8 +11,17 @@ import spoon.reflect.declaration.CtVariable;
  * Created by Adrian on 10/02/2017.
  */
 public class LTtoGT extends AbstractProcessor<CtBinaryOperator> {
+
+    @Override
+    public boolean isToBeProcessed(CtBinaryOperator element) {
+        if (element.getKind() == BinaryOperatorKind.LT)
+            if (!element.getParent().toString().startsWith("for"))
+                return true;
+
+        return false;
+    }
+
     public void process(CtBinaryOperator elem) {
-        if (elem.getKind() == BinaryOperatorKind.LT)
-            elem.setKind(BinaryOperatorKind.GT);
+        elem.setKind(BinaryOperatorKind.GT);
     }
 }
